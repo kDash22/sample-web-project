68cf16b92d07b0144610c1f0ef8b9e9de56ba4ec
kDash22 <kalna2295@gmail.com>
2026-06-01 14:37:01 +0530

    edited html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function showMessage(){
     document.getElementById("message").innerHTML = "ci/cd Pipeline Working !";
 }
+
+function showName(){
+    document.getElementById("name").innerHTML = "Kalana Dasanayaka";
+}
